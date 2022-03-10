9a8a6fa48df3b127bd4b1acdd1b3dfffb090d165
sushant-dasuri <sushant.dasuri@more.global>
2022-03-10 14:09:58 +0530

    Added require js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+/* document.addEventListener('DOMContentLoaded', function () {
     const chart = Highcharts.chart('container', {
         chart: {
             type: 'line'
@@ -22,4 +22,38 @@ document.addEventListener('DOMContentLoaded', function () {
             data: [10, 4, 8, 7, 6]
         }]
     });
+});
+ */
+
+// Load Highcharts
+var Highcharts = require('highcharts');
+// Alternatively, this is how to load Highstock. Highmaps and Highcharts Gantt are similar.
+// var Highcharts = require('highcharts/highstock');
+
+// Load the exporting module, and initialize it.
+require('highcharts/modules/exporting')(Highcharts);
+
+// Generate the chart
+Highcharts.chart('container', {
+    chart: {
+        type: 'line'
+    },
+    title: {
+        text: 'Working Days'
+    },
+    xAxis: {
+        categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+    },
+    yAxis: {
+        title: {
+            text: 'Working Hours'
+        }
+    },
+    series: [{
+        name: 'Jane',
+        data: [8, 10, 6, 8, 8]
+    }, {
+        name: 'John',
+        data: [10, 4, 8, 7, 6]
+    }]
 });
